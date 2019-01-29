e389edebc25083135290f46e14b2800b4abcc3ed
Brian Le <developer.cky@gmail.com>
2019-01-29 21:29:59 +0000

    Resolve modules

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['@babel/preset-env'],
+  plugins: [
+    [
+      'babel-plugin-module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          'db-utils': './utils/db-utils',
+        },
+      },
+    ],
+  ],
 };
--- api/clans/get.js
+++ api/clans/get.js
@@ -1,8 +1,8 @@
 const pool = require('../../db');
-const execQuery = require('../../db/utils/execQuery');
+const execQuery = require('db-utils/execQuery');
 
 module.exports = (_, res) => {
   execQuery(pool)('SELECT * FROM clans', null, rows => {
-    res.json(rows);
+    res.status(200).json(rows);
   });
 };
